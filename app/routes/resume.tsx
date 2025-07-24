a021237bb1b5d2199c3dc0c9ee96d1201324bb0e
app-css <abhinavpatil14062004@gmail.com>
2025-07-24 16:01:47 +0530

    final commit

--- app/routes/resume.tsx
+++ app/routes/resume.tsx
@@ -6,7 +6,7 @@ import ATS from "~/components/ATS";
 import Details from "~/components/Details";
 
 export const meta = () => ([
-    { title: 'Resumind | Review ' },
+    { title: 'Hirelyzer | Review ' },
     { name: 'description', content: 'Detailed overview of your resume' },
 ])
 
